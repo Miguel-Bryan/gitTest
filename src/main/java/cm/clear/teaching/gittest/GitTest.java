2907a8357a25fc57a800fa7264e7e1e5beff2d2a
Miguel-Bryan <kongnebryan945@gmail.com>
2024-06-20 16:36:47 +0100

    initial commit

---
+++ src/main/java/cm/clear/teaching/gittest/GitTest.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cm.clear.teaching.gittest;
+
+/**
+ *
+ * @author bryan
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("My Name is Bryan");
+        System.out.println("Before git add . ");
+    }
+}
